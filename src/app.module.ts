077712389526c6f3d7850c91d2867354bc8d47ab
Ayooluwa Adeleke <51849584+PolyMath-Coder@users.noreply.github.com>
2023-05-29 16:39:44 +0100

    chore: set up database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+require('dotenv').config();
+const databaseURL = process.env.DB_URL;
+const DB_LINK = databaseURL.toString();
+const DATABASE_NAME = process.env.DATABASE_NAME;
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(`${DB_LINK}`, {
+      dbName: `${DATABASE_NAME}`,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { config } from 'dotenv';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): object {
+    return {
+      status: true,
+      message: 'Welcome to Mitochron Hub...',
+    };
   }
 }
