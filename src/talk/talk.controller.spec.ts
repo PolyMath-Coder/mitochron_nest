cda2b5910668942118a48392c747255e87860d3b
Ayooluwa Adeleke <51849584+PolyMath-Coder@users.noreply.github.com>
2023-06-02 11:38:10 +0100

    feat: include unit tests

--- src/talk/talk.controller.spec.ts
+++ src/talk/talk.controller.spec.ts
@@ -1,9 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TalkController } from './talk.controller';
 import { TalkService } from './talk.service';
+import * as httpMocks from 'node-mocks-http';
 
 describe('TalkController', () => {
   let controller: TalkController;
+  let service: TalkService;
+
+  const mockReques = httpMocks.createRequest<any>();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
